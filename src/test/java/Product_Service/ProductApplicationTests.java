6f471a2c761b30c8b0b5e67eadd0fbe2e5b92470
mbandainnocent <innocent@igirerwanda.org>
2025-11-19 19:58:33 +0200

    pushing-changes

---
+++ src/test/java/Product_Service/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package Product_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
